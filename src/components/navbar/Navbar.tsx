98f202f0156993eea2dd66ff061ac8056e99822d
HomePC <xeeshanahmad348@gmail.com>
2024-07-19 22:32:52 +0500

    Add active state style to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,11 @@ import { FaHome, FaUser, FaBriefcase, FaEnvelope, FaComments } from 'react-icons
 const Navbar = () => {
   return (
     <div className='fixed right-0 top-1/2 transform -translate-y-1/2 md:flex flex-col items-center space-y-3  p-2 rounded-l-3xl hidden'>
-        <NavLink icon={<FaHome/>} path='/'></NavLink>
-        <NavLink icon={<FaUser/>} path='/about'></NavLink>
-        <NavLink icon={<FaBriefcase/>} path='/portfolio'></NavLink>
-        <NavLink icon={<FaEnvelope/>} path='/contact'></NavLink>
-        <NavLink icon={<FaComments/>} path='/blog'></NavLink>
+        <NavLink icon={<FaHome/>} path='/'/>
+        <NavLink icon={<FaUser/>} path='/about'/>
+        <NavLink icon={<FaBriefcase/>} path='/resume'/>
+        <NavLink icon={<FaEnvelope/>} path='/contact'/>
+        <NavLink icon={<FaComments/>} path='/blog'/>
     </div>
   )
 }
--- src/components/navbar/components/NavLink.tsx
+++ src/components/navbar/components/NavLink.tsx
@@ -1,11 +1,17 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 import { NavlinkInterface } from '@/interface/navlink_interface'
+import { usePathname } from 'next/navigation'
 
 const NavLink:React.FC<NavlinkInterface> = ({icon,path,className}) => {
+  const pathname=usePathname();
   return (
-    <Link href={path} className={`${className} text-yellow-500 text-2xl bg-zinc-700 w-14 h-14 mx-auto rounded-full flex justify-center items-center hover:bg-yellow-600 hover:text-white transition-colors`}>{icon}</Link> 
+    <Link href={path} className={`${pathname===path? `text-yellow-50 bg-yellow-600`:` text-yellow-500 bg-zinc-700`}  text-2xl  w-14 h-14 mx-auto rounded-full flex justify-center items-center hover:bg-yellow-600 hover:text-white transition-colors ${className}`}>{icon}</Link> 
   )
 }
-
+ const activestate:React.CSSProperties={
+  color:'white',
+  backgroundColor:'yellow',
+ }
 export default NavLink
